5f5dfd04cb5f5790c57fd1defcdde7c9612905c3
weiboqy <lanou@lanoudeMac-mini-5.local>
2016-03-25 16:34:58 +0800

    This is Test
    ..

---
+++ GitHubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubTest
+//
+//  Created by lanou on 16/3/25.
+//  Copyright © 2016年 xqy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitHubTest/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  GitHubTest
+//
+//  Created by lanou on 16/3/25.
+//  Copyright © 2016年 xqy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+    // This is Test
+    
+    
+    
+    
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
